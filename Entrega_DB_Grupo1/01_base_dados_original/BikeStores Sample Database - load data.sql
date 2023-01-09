2ab69fe257921d88f36c1b3c36cc4ffd46feb28a
carferreira <103536175+carferreira@users.noreply.github.com>
2023-01-09 12:14:12 +0000

    Actualização nos dados originais da base de dados
    Actualização nos dados originais da base de dados

--- Entrega_DB_Grupo1/01_base_dados_original/BikeStores Sample Database - load data.sql
+++ Entrega_DB_Grupo1/01_base_dados_original/BikeStores Sample Database - load data.sql
@@ -1798,8 +1798,8 @@ INSERT INTO sales.customers(first_name, last_name, phone, email, street, city, s
 
 -- stores
 
-INSERT INTO sales.stores(store_name, phone, email, street, city, state, zip_code) VALUES('Santa Cruz Bikes','(831) 476-4321','santacruz@bikes.shop','3700 Portola Drive', 'Santa Cruz','CA',95060),
-INSERT INTO sales.stores(store_name, phone, email, street, city, state, zip_code) VALUES('Baldwin Bikes','(516) 379-8888','baldwin@bikes.shop','4200 Chestnut Lane', 'Baldwin','NY',11432),
+INSERT INTO sales.stores(store_name, phone, email, street, city, state, zip_code) VALUES('Santa Cruz Bikes','(831) 476-4321','santacruz@bikes.shop','3700 Portola Drive', 'Santa Cruz','CA',95060);
+INSERT INTO sales.stores(store_name, phone, email, street, city, state, zip_code) VALUES('Baldwin Bikes','(516) 379-8888','baldwin@bikes.shop','4200 Chestnut Lane', 'Baldwin','NY',11432);
 INSERT INTO sales.stores(store_name, phone, email, street, city, state, zip_code) VALUES('Rowlett Bikes','(972) 530-5555','rowlett@bikes.shop','8000 Fairway Avenue', 'Rowlett','TX',75088);
 
 
